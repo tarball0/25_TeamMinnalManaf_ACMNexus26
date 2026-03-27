e05293b3464ce9c0d173d412a6fa90dfc44a1535
Fahad-uz <fahadmanaff@gmail.com>
2026-03-28 01:37:25 +0530

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -2,5 +2,17 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('desktopAPI', {
   pickFile: () => ipcRenderer.invoke('dialog:pickFile'),
-  runAnalysis: (filePath) => ipcRenderer.invoke('analysis:run', filePath)
+  runAnalysis: (filePath) => ipcRenderer.invoke('analysis:run', filePath),
+
+  onAutoScanResult: (callback) => {
+    const handler = (_event, payload) => callback(payload);
+    ipcRenderer.on('autoscan:complete', handler);
+    return () => ipcRenderer.removeListener('autoscan:complete', handler);
+  },
+
+  onAutoScanError: (callback) => {
+    const handler = (_event, payload) => callback(payload);
+    ipcRenderer.on('autoscan:error', handler);
+    return () => ipcRenderer.removeListener('autoscan:error', handler);
+  }
 });
